e2cd30b013ecc7d4e7318aee1299e7b56bfdbeb0
Ubuntu <ubuntu@ip-172-31-45-57.eu-north-1.compute.internal>
2025-07-12 18:49:19 +0000

    Webhook test: updated message in App.java

--- app.java
+++ app.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Sourabh! CI/CD pipeline is working.");
+        System.out.println("Hello, Sourabh! Webhook trigger test.");
     }
 }
